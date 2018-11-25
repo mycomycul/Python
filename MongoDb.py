e920691af15089540a044e4ac7e630bd6cfb1a88
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-11-24 22:10:49 -0800

    Initial Create DB and insert column code

--- MongoDb.py
+++ MongoDb.py
@@ -1,6 +1,13 @@
-# import pymongo
+import pymongo
 
-# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-# mydb = myclient["mydatabase"]
-import sys
-print(sys.executable)
+myclient = pymongo.MongoClient("127.0.0.1", 27017)
+mydb = myclient["mydatabase"]
+
+mycol = mydb["customers"]
+
+mydict = {"name" : "John","address":"Highway 37"}
+
+x = mycol.insert_one(mydict)
+print(x.inserted_id)
+
+# print(myclient.list_database_names())e
