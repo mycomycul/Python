d1e4db7ff76c1ce569e0d27484947dbaaec501de
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-11-25 18:32:44 -0800

    Add print collection list

--- MongoDb.py
+++ MongoDb.py
@@ -3,6 +3,10 @@ import pymongo
 myclient = pymongo.MongoClient("127.0.0.1", 27017)
 #Create Database
 mydb = myclient["mydatabase"]
+
+collectionlist = mydb.list_collection_names()
+print(collectionlist)
+
 #Create Collection in Database
 mycol = mydb["customers"]
  
@@ -27,12 +31,27 @@ mylist = [
 ]
 x = mycol.insert_many(mylist)
 
+#Create a list with ids provided
+# mylistwithids = [
+#     {"_id":1,"name":"John","address":"highway 37"},
+#     {"_id":2,"name":"Jill","address":"highway 37"},
+#     {"_id":3,"name":"Jack","address":"highway 37"},
+#     {"_id":4,"name":"Jane","address":"highway 37"},
+#     {"_id":5,"name":"Jim","address":"highway 37"},
+#     {"_id":6,"name":"Joan","address":"highway 37"},
+# ]
+
+# x = mycol.insert_many(mylistwithids)
+
+
 print(x.inserted_ids)
 
-#Pull all database names into list and check if the specified database exists
-dblist = myclient.list_database_names()
-if "mydatabase" in dblist:
-    print("The database exists")
+# #Pull all database names into list and check if the specified database exists
+# dblist = myclient.list_database_names()
+# if "mydatabase" in dblist:
+#     print("The database exists")
+
+
 
 
 
